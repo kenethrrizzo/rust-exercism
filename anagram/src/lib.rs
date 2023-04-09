b467a5b4b5c73324ea470ded2ac17262f9e6b7ff
kenethriera <kenethrr@outlook.com>
2023-04-09 00:45:09 -0500

    Se agrega solución a anagram

---
+++ anagram/src/lib.rs
@@ -0,0 +1,26 @@
+use std::collections::HashSet;
+
+pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let sorted_word = sort_and_up_word(word);
+
+    let mut set: HashSet<&'a str> = HashSet::new();
+    for anagram in possible_anagrams.iter() {
+        if eq_ignore_case(word, anagram) {
+            continue;
+        }
+        if sorted_word == sort_and_up_word(anagram) {
+            set.insert(anagram);
+        }
+    }
+    set
+}
+
+fn sort_and_up_word(word: &str) -> String {
+    let mut chars: Vec<char> = word.to_uppercase().chars().collect();
+    chars.sort_unstable();
+    chars.into_iter().collect()
+}
+
+fn eq_ignore_case(first_string: &str, second_string: &str) -> bool {
+    first_string.to_lowercase() == second_string.to_lowercase()
+}
